8b85a2aba2a7e4516960303c3904acc5e38b79d3
Deviprasad Sahu <sahudebiprasad94@gmail.com>
2024-02-03 20:13:14 +0530

    This is my learning about Conditional Statements

---
+++ conditional.js
@@ -0,0 +1,110 @@
+//Conditional Statements
+
+//if statement
+// let age = 15;
+
+// if(age >= 18) {
+//     console.log("You can vote");
+// }
+
+// if(age < 18) {
+//     console.log("You cannot vote");
+// }
+
+
+//if-else statement
+// let mode = "dark";
+// let color;
+// 
+// if(mode === "dark") {
+    // color = "black";
+// }
+// else {
+    // color = "white";
+// }
+// console.log(color);
+// 
+// let age = 25;
+// 
+// if(age >= 18) {
+    // console.log("vote");
+// }
+// else {
+    // console.log("Not vote")
+// }
+// 
+// let num = 4;
+// 
+// if(num % 2 === 0) {
+    // console.log("Number is even");
+// }
+// else {
+    // console.log("Number is odd");
+// }
+
+
+//else-if statement
+// if(age < 18) {
+//     console.log("Junior");
+// }
+// else if (age > 60) {
+//     console.log("Senior");
+// }
+// else {
+//     console.log("Middle");
+// }
+
+let mode = "dark";
+let color;
+
+if(mode === "dark") {
+    color = "black";
+}
+else if (mode === "blue") {
+    color = "blue";
+}
+else if (mode === pink) {
+    color = "pink";
+}
+else {
+    color = "white";
+}
+
+console.log(color);
+
+
+//Ternary Statement
+
+let age = 25;
+
+let result = age >= 18 ? "adult" : "not adult";  //simpler, compact if-else
+
+
+//Practice question 1
+// let num = prompt ("Enter a number");
+
+// if(num % 5 === 0) {
+//     console.log(num, "is equal to 5");
+// } else {
+//     console.log("Number is not equal to 5");
+// }
+
+
+//Practice question 2
+
+let score = prompt("Enter your score (0-100):");
+let grade;
+
+if(score >= 90 && score <= 100) {
+    grade = "A";
+} else if(score >= 70 && score <=89) {
+    grade = "B";
+} else if(score >= 60 && score <= 69) {
+    grade = "C";
+} else if(score >= 0 && score <= 59) {
+    grade = "D";
+} else if(score >= 0 && score <= 49) {
+    grade = "F";
+}
+
+console.log("According to your scores, your grade is", grade);
